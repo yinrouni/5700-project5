af59aecf33826b92a5cd917d5b94e384fba55204
rouni.yin <yin.ro@husky.neu.edu>
2020-12-03 20:12:42 -0500

    trim dns

---
+++ DNSPacket.py
@@ -0,0 +1,73 @@
+import socketserver
+import struct
+import socket as socketlib
+import traceback
+import dnslib
+import traceback
+
+# DNS Query
+class DNSQuery:
+    def __init__(self, data):
+        i = 1
+        self.name = ''
+        self.data = data
+
+        while True:
+            d = data[i]
+            if d == 0:
+                break
+            if d < 32:
+                self.name = self.name + '.'
+            else:
+                self.name = self.name + chr(d)
+            i = i + 1
+        self.querybytes = data[0:i + 1]
+        (self.type, self.classify) = struct.unpack('!HH', data[i + 1:i + 5])
+        self.len = i + 5
+        # print(data[self.len:])
+
+    def pack(self):
+        return self.querybytes + struct.pack('!HH', self.type, self.classify), self.data[self.len:]
+
+
+# DNS Answer RRS
+# this class is also can be use as Authority RRS or Additional RRS
+class DNSAnswer:
+    def __init__(self, ip):
+        self.name = 49164
+        self.type = 1
+        self.classify = 1
+        self.timetolive = 190
+        self.datalength = 4
+        self.ip = ip
+
+    def pack(self):
+        res = struct.pack('!HHHLH', self.name, self.type, self.classify, self.timetolive, self.datalength)
+        s = self.ip.split('.')
+        res = res + struct.pack('BBBB', int(s[0]), int(s[1]), int(s[2]), int(s[3]))
+        return res
+
+
+# DNS frame
+# must initialized by a DNS query frame
+class DNSFrame:
+    def __init__(self, data):
+        (self.id, self.flags, self.quests, self.answers, self.author, self.addition) = struct.unpack('>HHHHHH',
+                                                                                                     data[0:12])
+        self.query = DNSQuery(data[12:])
+
+    def getname(self):
+        return self.query.name
+
+    def setip(self, ip):
+        self.answer = DNSAnswer(ip)
+        self.answers = 1
+        self.flags = 33152
+
+    def getbytes(self):
+        res = struct.pack('!HHHHHH', self.id, self.flags, self.quests, self.answers, self.author, self.addition)
+        res = res + self.query.pack()[0]
+        if self.answers != 0:
+            res = res + self.answer.pack()
+        return res + self.query.pack()[1]
+
--- DNSserver.py
+++ DNSserver.py
@@ -1,25 +1,68 @@
 # https://www.cnblogs.com/dongkuo/p/6714071.html
 # http://c.biancheng.net/view/6457.html
 import socket
+import DNSPacket
+import traceback
+# import dnslib
 
 class DNSserver:
     def __init__(self, port):
         self.server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # udp
         self.server.bind(('', port))
-
+        self.namemap = {}
 
     def serve_forever(self):
-
         while True:
             # dns request
             data, addr = self.server.recvfrom(1024)
 
+            dns = DNSPacket.DNSFrame(data)
+
+            if (dns.query.type == 1):
+                # If this is query a A record, then response it
+
+                name = dns.getname()
+                toip = None
+                ifrom = "map"
+                if self.namemap.__contains__(name):
+                    # If have record, response it
+                    # dns.setip(namemap[name])
+                    # socket.sendto(dns.getbytes(), self.client_address)
+                    toip = self.namemap[name]
+                elif self.namemap.__contains__('*'):
+                    # Response default address
+                    # dns.setip(namemap['*'])
+                    # socket.sendto(dns.getbytes(), self.client_address)
+                    toip = self.namemap['*']
+                else:
+                    # ignore it
+                    # socket.sendto(data, self.client_address)
+                    # socket.getaddrinfo(name,0)
+                    try:
+                        toip = socket.getaddrinfo(name, 0)[0][4][0]
+                        ifrom = "sev"
+                    # namemap[name] = toip
+                    # print socket.getaddrinfo(name,0)
+                    except Exception as e:
+                        traceback.print_exc()
+                        print('get ip fail')
+                if toip:
+                    dns.setip(toip)
+                print('%s: %s-->%s (%s)' % (addr[0], name, toip, ifrom))
+                # pprint.pprint(t1.decode_dns_message(dns.getbytes()))
+                # print(dnslib.DNSRecord.parse(dns.getbytes()))
+                self.server.sendto(dns.getbytes(), addr)
+            else:
+                # If this is not query a A record, ignore it
+                self.server.sendto(data, addr)
 
 
-
-    def send(self, data, addr):
-
-
-
-
-        self.serer.sendto(dns_resp, addr)
+    def add_name(self, name, ip):
+        self.namemap[name] = ip
+if __name__ == "__main__":
+    sev = DNSserver(port= 53)
+    sev.add_name('www.aa.com', '192.168.0.1')  # add a A record
+    sev.add_name('www.bb.com', '192.168.0.2')  # add a A record
+    # sev.addname('*', '0.0.0.0') # default address
+    print('listening...')
+    sev.serve_forever()  # start DNS server
