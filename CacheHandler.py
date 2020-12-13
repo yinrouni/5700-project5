967b3d03cf4fa5032db9ac80eeb2dd329587bf65
mrchs0119 <sun.chih@husky.neu.edu>
2020-12-13 11:03:18 -0500

    scritp and document

--- CacheHandler.py
+++ CacheHandler.py
@@ -5,16 +5,32 @@ import os
 import hashlib
 import gzip
 
+# cache file's limited size
 MAX_SIZE = 10 * 1024 * 1024
 
 
 def get_hash_path(path):
+    """
+    Convert path to hash format
+    :param path: the path in the request
+    :return
+    The path of hash format
+    """
     m = hashlib.md5()
     m.update(path)
     return m.hexdigest()
 
 
 class CacheHandler:
+    """
+    This is cache handler of http server which implemented a cache replacement strategy
+    It provides following methods:
+    load_cache(): initial the cache directory.
+    get(path): if the path already in the cache directory, increase that file's hit num and return the content.
+    set(path, data): if the path is not in the cache directory, calculate the data size
+    and set it to the cache directory. if cache file is full, replacement the file in the cache.
+
+    """
 
     def __init__(self):
 
@@ -24,14 +40,26 @@ class CacheHandler:
         self.cached_data = self.load_cache()  # (hashed_filename, hitcount)
 
     def load_cache(self):
+        """
+        initial the cache directory
+        :return
+        The information of cache directory
+        """
         with self.lock:
             if os.path.exists(self.cache_dir):
+                # set each file hit num as 1 if there are file in cache directory when http server start runing
                 return map(lambda x: (x, 1), os.listdir(self.cache_dir))
             else:
                 os.mkdir(self.cache_dir)
                 return []
 
     def get(self, path):
+        """
+        if the path already in the cache directory, increase that file's hit num and return the content.
+        :param path: the path of request
+        :return
+        The content of path
+        """
         with self.lock:
             file_name = get_hash_path(path)
 
@@ -55,11 +83,14 @@ class CacheHandler:
             return None
 
     def set(self, path, data):
+        """
+        if the path is not in the cache directory, calculate the data size
+        :param data: the content from origin server
+        :param path: the path of request
+        """
         with self.lock:
             file_name = get_hash_path(path)
 
-            # if file_name in self.cached_data:
-            #     self.cached_data.remove(file_name)
             temp_file = gzip.open(file_name + '.temp', 'wb')
             temp_file.write(data)
             temp_file.close()
@@ -81,6 +112,12 @@ class CacheHandler:
                 print('cache update ------', self.cached_data)
 
     def is_full(self, data_size):
+        """
+        Check the cache directory usage
+        :param data_size: the size of temp file which try to add to cache directory
+        :return
+        The boolean
+        """
         cache = os.listdir(self.cache_dir)
         total = 0
         for f in cache:
